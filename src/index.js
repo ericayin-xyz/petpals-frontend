6813cb17cd481a752e3f3f6868b5927b5d93fafb
luyueyin <12898@coderacademy.edu.au>
2023-03-06 12:53:31 +1100

    Change axios baseURL to deployed backed API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = "https://petpals-backend-production-8859.up.railway.app" 
+axios.defaults.baseURL = "https://petpals-backend-production.up.railway.app" 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
